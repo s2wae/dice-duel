e4a229baf8bbde7af40aae6213ecb82b9acdfaf0
s2wae <iantxh@gmail.com>
2024-10-01 02:16:11 +0800

    shop fix

--- scripts/shop_ui.gd
+++ scripts/shop_ui.gd
@@ -17,24 +17,42 @@ func _process(delta):
 	pass
 
 
-func _on_button_pressed():
-	$CanvasLayer/GridContainer/TextureRect.modulate.a = 0
-	$CanvasLayer/GridContainer/TextureRect/Button.visible = false
-	print("hello")
-	
-
-
 func _on_reroll_button_pressed():
-	%Button.visible = true
+	$CanvasLayer/GridContainer/shopslot1/Button1.visible = true
+	$CanvasLayer/GridContainer/shopslot2/Button2.visible = true
+	$CanvasLayer/GridContainer/shopslot3/Button3.visible = true
+	$CanvasLayer/GridContainer/shopslot4/Button4.visible = true
+	$CanvasLayer/GridContainer/shopslot5/Button5.visible = true
 	
-	$CanvasLayer/GridContainer/TextureRect.modulate.a = 1
-	$CanvasLayer/GridContainer/TextureRect2.modulate.a = 1
-	$CanvasLayer/GridContainer/TextureRect3.modulate.a = 1
-	$CanvasLayer/GridContainer/TextureRect4.modulate.a = 1
-	$CanvasLayer/GridContainer/TextureRect5.modulate.a = 1
+	$CanvasLayer/GridContainer/shopslot1.modulate.a = 1
+	$CanvasLayer/GridContainer/shopslot2.modulate.a = 1
+	$CanvasLayer/GridContainer/shopslot3.modulate.a = 1
+	$CanvasLayer/GridContainer/shopslot4.modulate.a = 1
+	$CanvasLayer/GridContainer/shopslot5.modulate.a = 1
 	
-	$CanvasLayer/GridContainer/TextureRect.texture = array.pick_random()
-	$CanvasLayer/GridContainer/TextureRect2.texture = array.pick_random()
-	$CanvasLayer/GridContainer/TextureRect3.texture = array.pick_random()
-	$CanvasLayer/GridContainer/TextureRect4.texture = array.pick_random()
-	$CanvasLayer/GridContainer/TextureRect5.texture = array.pick_random()
+	$CanvasLayer/GridContainer/shopslot1.texture = array.pick_random()
+	$CanvasLayer/GridContainer/shopslot2.texture = array.pick_random()
+	$CanvasLayer/GridContainer/shopslot3.texture = array.pick_random()
+	$CanvasLayer/GridContainer/shopslot4.texture = array.pick_random()
+	$CanvasLayer/GridContainer/shopslot5.texture = array.pick_random()
+
+
+func _on_button_1_pressed():
+	$CanvasLayer/GridContainer/shopslot1.modulate.a = 0
+	$CanvasLayer/GridContainer/shopslot1/Button1.visible = false
+
+func _on_button_2_pressed():
+	$CanvasLayer/GridContainer/shopslot2.modulate.a = 0
+	$CanvasLayer/GridContainer/shopslot2/Button2.visible = false
+
+func _on_button_3_pressed():
+	$CanvasLayer/GridContainer/shopslot3.modulate.a = 0
+	$CanvasLayer/GridContainer/shopslot3/Button3.visible = false
+
+func _on_button_4_pressed():
+	$CanvasLayer/GridContainer/shopslot4.modulate.a = 0
+	$CanvasLayer/GridContainer/shopslot4/Button4.visible = false
+
+func _on_button_5_pressed():
+	$CanvasLayer/GridContainer/shopslot5.modulate.a = 0
+	$CanvasLayer/GridContainer/shopslot5/Button5.visible = false
