091ba65d5f1045b81e869a0bdfa35328b69477e4
s2wae <iantxh@gmail.com>
2024-10-01 02:49:13 +0800

    shop updates

--- scripts/shop_ui.gd
+++ scripts/shop_ui.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export var coins = 10
+
 var briar = preload("res://placeholders/Briar.png")
 var gwen = preload("res://placeholders/Gwen.png")
 var milio = preload("res://placeholders/Milio.png")
@@ -9,27 +11,7 @@ var varus = preload("res://placeholders/Varus.png")
 var array = [briar, gwen, milio, teemo, varus] 
 
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_reroll_button_pressed():
-	$CanvasLayer/GridContainer/shopslot1/Button1.visible = true
-	$CanvasLayer/GridContainer/shopslot2/Button2.visible = true
-	$CanvasLayer/GridContainer/shopslot3/Button3.visible = true
-	$CanvasLayer/GridContainer/shopslot4/Button4.visible = true
-	$CanvasLayer/GridContainer/shopslot5/Button5.visible = true
-	
-	$CanvasLayer/GridContainer/shopslot1.modulate.a = 1
-	$CanvasLayer/GridContainer/shopslot2.modulate.a = 1
-	$CanvasLayer/GridContainer/shopslot3.modulate.a = 1
-	$CanvasLayer/GridContainer/shopslot4.modulate.a = 1
-	$CanvasLayer/GridContainer/shopslot5.modulate.a = 1
-	
+	$CanvasLayer/coins.text = "Coins: " + str(coins)
 	$CanvasLayer/GridContainer/shopslot1.texture = array.pick_random()
 	$CanvasLayer/GridContainer/shopslot2.texture = array.pick_random()
 	$CanvasLayer/GridContainer/shopslot3.texture = array.pick_random()
@@ -37,22 +19,67 @@ func _on_reroll_button_pressed():
 	$CanvasLayer/GridContainer/shopslot5.texture = array.pick_random()
 
 
+func _process(delta):
+	pass
+
+
+func _on_reroll_button_pressed():
+	
+	if coins > 0:
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
+		$CanvasLayer/GridContainer/shopslot1/Button1.visible = true
+		$CanvasLayer/GridContainer/shopslot2/Button2.visible = true
+		$CanvasLayer/GridContainer/shopslot3/Button3.visible = true
+		$CanvasLayer/GridContainer/shopslot4/Button4.visible = true
+		$CanvasLayer/GridContainer/shopslot5/Button5.visible = true
+		
+		$CanvasLayer/GridContainer/shopslot1.modulate.a = 1
+		$CanvasLayer/GridContainer/shopslot2.modulate.a = 1
+		$CanvasLayer/GridContainer/shopslot3.modulate.a = 1
+		$CanvasLayer/GridContainer/shopslot4.modulate.a = 1
+		$CanvasLayer/GridContainer/shopslot5.modulate.a = 1
+		
+		$CanvasLayer/GridContainer/shopslot1.texture = array.pick_random()
+		$CanvasLayer/GridContainer/shopslot2.texture = array.pick_random()
+		$CanvasLayer/GridContainer/shopslot3.texture = array.pick_random()
+		$CanvasLayer/GridContainer/shopslot4.texture = array.pick_random()
+		$CanvasLayer/GridContainer/shopslot5.texture = array.pick_random()
+
+
 func _on_button_1_pressed():
-	$CanvasLayer/GridContainer/shopslot1.modulate.a = 0
-	$CanvasLayer/GridContainer/shopslot1/Button1.visible = false
+	if coins > 0:
+		$CanvasLayer/GridContainer/shopslot1.modulate.a = 0
+		$CanvasLayer/GridContainer/shopslot1/Button1.visible = false
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
+
 
 func _on_button_2_pressed():
-	$CanvasLayer/GridContainer/shopslot2.modulate.a = 0
-	$CanvasLayer/GridContainer/shopslot2/Button2.visible = false
+	if coins > 0:
+		$CanvasLayer/GridContainer/shopslot2.modulate.a = 0
+		$CanvasLayer/GridContainer/shopslot2/Button2.visible = false
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
 
 func _on_button_3_pressed():
-	$CanvasLayer/GridContainer/shopslot3.modulate.a = 0
-	$CanvasLayer/GridContainer/shopslot3/Button3.visible = false
+	if coins > 0:
+		$CanvasLayer/GridContainer/shopslot3.modulate.a = 0
+		$CanvasLayer/GridContainer/shopslot3/Button3.visible = false
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
 
 func _on_button_4_pressed():
-	$CanvasLayer/GridContainer/shopslot4.modulate.a = 0
-	$CanvasLayer/GridContainer/shopslot4/Button4.visible = false
+	if coins > 0:
+		$CanvasLayer/GridContainer/shopslot4.modulate.a = 0
+		$CanvasLayer/GridContainer/shopslot4/Button4.visible = false
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
 
 func _on_button_5_pressed():
-	$CanvasLayer/GridContainer/shopslot5.modulate.a = 0
-	$CanvasLayer/GridContainer/shopslot5/Button5.visible = false
+	if coins > 0:
+		$CanvasLayer/GridContainer/shopslot5.modulate.a = 0
+		$CanvasLayer/GridContainer/shopslot5/Button5.visible = false
+		coins -= 1
+		$CanvasLayer/coins.text = "Coins: " + str(coins)
+
